d79967efee6f93519cdf6efc9df0c5204cacd463
hongkai97 <79686141+hongkai97@users.noreply.github.com>
2021-06-29 17:33:59 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 basic.forever(function () {
     basic.pause(500)
-    if (pins.analogReadPin(AnalogPin.P1) < 50) {
+    if (pins.analogReadPin(AnalogPin.P2) < 50) {
         music.setVolume(127)
-        music.playTone(349, music.beat(BeatFraction.Whole))
+        music.playTone(247, music.beat(BeatFraction.Whole))
+        music.playTone(220, music.beat(BeatFraction.Whole))
+        music.playTone(196, music.beat(BeatFraction.Whole))
         basic.showLeds(`
             . # . # .
             . # . # .
