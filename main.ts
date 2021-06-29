5422c8490fca2bb8339450e7528143a862db72f5
hongkai97 <79686141+hongkai97@users.noreply.github.com>
2021-06-29 16:32:09 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 basic.forever(function () {
-    serial.writeValue("x", pins.analogReadPin(AnalogPin.P1))
     basic.pause(500)
     if (pins.analogReadPin(AnalogPin.P1) < 50) {
         music.setVolume(127)
-        soundExpression.sad.play()
+        music.playTone(349, music.beat(BeatFraction.Whole))
         basic.showLeds(`
             . # . # .
             . # . # .
